f2b8490d53ed19a8ab97cc080cced3ca63f7965e
sakhawat777 <biplobbms@gmail.com>
2021-11-25 13:01:55 +0600

    Updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React, { createContext, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Shipment from './components/Shipment/Shipment';
-// Here createContext
-export const CategoryContext = createContext();
+
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		// For State Management by ContextApi
-		<CategoryContext.Provider value={count}>
+		// For State Management by props
+		<div>
 			<Header count={count} setCount={setCount}></Header>
 			<Home count={count}></Home>
 			<Shipment></Shipment>
-		</CategoryContext.Provider>
-		// For State Management by props
-		// <div>
-		// 	<Header count={count} setCount={setCount}></Header>
-		// 	<Home count={count}></Home>
-		// 	<Shipment></Shipment>
-		// </div>
+		</div>
 	);
 }
 
--- src/components/CategoryDetails/CategoryDetails.js
+++ src/components/CategoryDetails/CategoryDetails.js
@@ -1,13 +1,10 @@
-import React, { useContext } from 'react';
-import { CategoryContext } from '../../App';
+import React from 'react';
 
 const CategoryDetails = (props) => {
-	const category = useContext(CategoryContext);
 	const { count } = props;
 	return (
 		<div>
 			<h4>This is Category Details:{count} </h4>
-			<h3>Selected Category: {category}</h3>
 		</div>
 	);
 };
